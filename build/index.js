d2f191ca2fc621228da86c994fff324649fba6f6
yuli349 <yuli.belitskaya349@gmail.com>
2021-03-20 20:37:58 +0300

    added define function

--- build/index.js
+++ build/index.js
@@ -373,3 +373,21 @@ define("index", ["require", "exports", "store", "slides/leaders", "slides/vote",
     }
     exports.prepareData = prepareData;
 });
+var modules;
+function define(name, args, cb) {
+    if (!modules) modules = {};
+    if (typeof window === "undefined") {
+        var moduleExports = {};
+        var actualArgs = args.map(function(arg) {
+            if (arg === "require") return require;
+            if (arg === "exports") return moduleExports;
+            return modules[arg];
+        });
+
+        cb.apply(null, actualArgs);
+
+        modules[name] = moduleExports;
+    } else {
+        cb(function() {}, window);
+    }
+}
